c4d96fe0bb0ccce27461210ed69e1b14d673ad0d
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-22 12:18:06 +0300

    Added unit tests setup `RemoteFeedLoaderTests`

---
+++ EssentialFeed/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,10 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alexander Nikolaychuk on 22.09.2022.
+//
+
+import XCTest
+
+class RemoteFeedLoaderTests: XCTestCase {}
