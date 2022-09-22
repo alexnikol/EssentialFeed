1d542ceaa345a072d435ebcf64f998dbbc0a7d8c
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-22 11:14:19 +0300

    Create `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alexander Nikolaychuk on 22.09.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
