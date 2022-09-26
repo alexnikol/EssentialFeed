227903c0d9c0752afdeb3438c0229e3f51eed5c8
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-26 15:05:12 +0300

    Moved `LocalFeedItem` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,17 @@
+// Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
