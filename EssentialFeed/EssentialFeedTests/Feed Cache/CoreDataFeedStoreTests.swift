6cc4d16a1647d1f18a4c642c48c92459c3729a8d
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-28 12:33:25 +0300

    CoreDataFeedStore.insert() delivers no error on non-empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -35,7 +35,11 @@ class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
-    func test_insert_deliversNoErrorOnNonEmptyCache() {}
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {}
     
