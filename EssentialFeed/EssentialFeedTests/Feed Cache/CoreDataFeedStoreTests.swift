a1c333709d27189961d82d36c3918c7dad14b9c7
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-28 12:32:12 +0300

    CoreDataFeedStore.retrieve() has no side-effects on non-empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -10,47 +10,51 @@ class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
-
+    
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
-
+    
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
-
+        
         assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
-
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {}
-
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    }
+    
     func test_insert_deliversNoErrorOnEmptyCache() {}
-
+    
     func test_insert_deliversNoErrorOnNonEmptyCache() {}
-
+    
     func test_insert_overridesPreviouslyInsertedCacheValues() {}
-
+    
     func test_delete_deliversNoErrorOnEmptyCache() {}
-
+    
     func test_delete_hasNoSideEffectsOnEmptyCache() {}
-
+    
     func test_delete_deliversNoErrorOnNonEmptyCache() {}
-
+    
     func test_delete_emptiesPreviouslyInsertedCache() {}
-
+    
     func test_storeSideEffects_runSerially() {}
     
     func test_retrieve_deliversFailureOnRetrievalError() {}
-
+    
     func test_retrieve_hasNoSideEffectsOnFailure() {}
-
+    
     func test_insert_deliversErrorOnInsertionError() {}
-
+    
     func test_insert_hasNoSideEffectsOnInsertionError() {}
-
+    
     func test_delete_deliversErrorOnDeletionError() {}
-
+    
     func test_delete_hasNoSideEffectsOnDeletionError() {}
     
     // MARK: - Helpers
