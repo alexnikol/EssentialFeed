1d3ffc688fd64d91ef7334d6acc1617ec216e1ce
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-28 12:07:42 +0300

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,15 @@
+// Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
+
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+}
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -5,21 +5,11 @@ import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     
-    func test_retrieve_deliversFailureOnRetrievalError() {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
-
-    func test_retrieve_hasNoSideEffectsOnFailure() {}
-
-    func test_insert_deliversErrorOnInsertionError() {}
-
-    func test_insert_hasNoSideEffectsOnInsertionError() {}
-
-    func test_delete_deliversErrorOnDeletionError() {}
-
-    func test_delete_hasNoSideEffectsOnDeletionError() {}
-
-    func test_retrieve_deliversEmptyOnEmptyCache() {}
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {}
 
@@ -42,4 +32,24 @@ class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     func test_delete_emptiesPreviouslyInsertedCache() {}
 
     func test_storeSideEffects_runSerially() {}
+    
+    func test_retrieve_deliversFailureOnRetrievalError() {}
+
+    func test_retrieve_hasNoSideEffectsOnFailure() {}
+
+    func test_insert_deliversErrorOnInsertionError() {}
+
+    func test_insert_hasNoSideEffectsOnInsertionError() {}
+
+    func test_delete_deliversErrorOnDeletionError() {}
+
+    func test_delete_hasNoSideEffectsOnDeletionError() {}
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
