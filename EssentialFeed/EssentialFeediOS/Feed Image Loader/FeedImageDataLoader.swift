1eda21249264d7c649328c9ff205c5f3dbfa4abd
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-29 13:32:46 +0300

    Structure UI EssentialFeediOS to MVC

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,13 @@
+// Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
