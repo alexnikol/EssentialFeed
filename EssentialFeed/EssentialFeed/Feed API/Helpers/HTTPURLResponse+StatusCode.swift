af244883ad08c870af0fca39e6df810f6d66d253
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-03 16:07:18 +0300

    Extract duplicate HTTPURLResponse.statusCode == 200 validation into a `isOK` helper method

---
+++ EssentialFeed/EssentialFeed/Feed API/Helpers/HTTPURLResponse+StatusCode.swift
@@ -0,0 +1,11 @@
+// Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
+
+import Foundation
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+    
+    var isOK: Bool {
+        return statusCode == HTTPURLResponse.OK_200
+    }
+}
