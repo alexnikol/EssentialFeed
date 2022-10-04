3e0cd231ee821ec1df451fd6f52d0ad50d284a38
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-04 15:23:42 +0300

    Composed iOS App

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -1,6 +1,8 @@
 // Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -8,5 +10,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let httpClient = URLSessionHTTPClient(session: URLSession.shared)
+        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: httpClient)
+        let remoteImageDataLoader = RemoteFeedImageDataLoader(client: httpClient)
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageDataLoader
+        )
+        
+        window?.rootViewController = feedViewController
     }
 }
