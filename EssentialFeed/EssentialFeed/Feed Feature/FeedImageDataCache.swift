884495044984125333afa29ace2aff71d2f8bdf3
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-06 14:15:57 +0300

    Move FeedImageDataCache to the Feed Feature production group

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,9 @@
+// Copyright © 2022 Oleksandr Nikolaichuk. All rights reserved.
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
